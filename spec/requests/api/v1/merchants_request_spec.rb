ed79da95fa1a2eac4a8981bf583c7451bf6bed76
DanHutch <danhutch5280@gmail.com>
2018-11-27 17:53:23 -0700

    merchants serialized

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,7 +10,7 @@ describe "Merchants API" do
 
 		merchants = JSON.parse(response.body)
 
-		expect(merchants.count).to eq(3)
+		expect(merchants["data"].count).to eq(3)
 
 	end
 
@@ -22,7 +22,7 @@ describe "Merchants API" do
 		merchant = JSON.parse(response.body)
 
 		expect(response).to be_successful
-		expect(merchant["id"]).to eq(id)
+		expect(merchant["data"]["id"]).to eq(id.to_s)
 
 	end
 
