48a58bf54248d5ac3cce3338de30b2c53d9136b1
DanHutch <danhutch5280@gmail.com>
2018-11-30 18:24:59 -0700

    implemented and tested top items

--- spec/requests/api/v1/most_sold_items_request_spec.rb
+++ spec/requests/api/v1/most_sold_items_request_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 describe "Most Sold Items API" do 
 
-	xit "can get the most sold items" do
-		custy = create(:customer)
+	it "can get the most sold items" do
 		item_1, item_2, item_3, item_4 = create_list(:item, 4)
-		allow(Customer).to receive(:most_sold).with("2").and_return([item_1, item_4])
+		allow(Item).to receive(:most_sold).with("2").and_return([item_1, item_4])
 
 		get "/api/v1/items/most_items?quantity=2"
 
