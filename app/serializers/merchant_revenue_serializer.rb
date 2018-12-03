251c17b2fef788a63b103f039d3bb198dbb73e1d
DanHutch <danhutch5280@gmail.com>
2018-12-02 19:08:54 -0700

    refactored to pass spec harness

--- app/serializers/merchant_revenue_serializer.rb
+++ app/serializers/merchant_revenue_serializer.rb
@@ -1,6 +1,6 @@
 class MerchantRevenueSerializer
   include FastJsonapi::ObjectSerializer
   attributes :revenue do |object|
-   '%.2f' % (object.revenue / 100)
+   '%.2f' % (object.revenue / 100.00)
   end
 end
